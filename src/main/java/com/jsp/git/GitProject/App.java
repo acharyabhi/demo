859910e1dc22c4fbfbfd8f7ee17efd48d5c40a99
acharyabhi <abhishekracharya@gmail.com>
2023-12-11 14:26:12 +0530

    change

--- src/main/java/com/jsp/git/GitProject/App.java
+++ src/main/java/com/jsp/git/GitProject/App.java
@@ -11,3 +11,11 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+/*
+ * 1.git init 2. 3.git status git add src pom.xml git status
+ */
+ //1.initializing local repository
+//2
+//3.staging the changed that has to be added to git  local repository
+//4.committing
+// create a branch command git checkout -n abhi (name)
