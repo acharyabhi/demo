340db45a3e11bdc485480eb3123047a4cfd87767
acharyabhi <abhishekracharya@gmail.com>
2023-12-11 14:02:04 +0530

    project created

---
+++ src/main/java/com/jsp/git/GitProject/App.java
@@ -0,0 +1,13 @@
+package com.jsp.git.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
