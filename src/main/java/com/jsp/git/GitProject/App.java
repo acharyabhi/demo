5436bdfb38769b7158f7dc3b10405eb222357d7b
acharyabhi <abhishekracharya@gmail.com>
2023-12-11 14:44:05 +0530

    change made by abhi

--- src/main/java/com/jsp/git/GitProject/App.java
+++ src/main/java/com/jsp/git/GitProject/App.java
@@ -7,8 +7,7 @@ package com.jsp.git.GitProject;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {System.out.println( "Hello World!" );
     }
 }
 
